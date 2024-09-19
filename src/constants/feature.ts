0f650dcd78e18325b72be650539e6d1d7eb2ffe3
Hilaladiii <abdulhilal242@gmail.com>
2024-09-19 17:07:54 +0700

    feat : add constant for feature website

---
+++ src/constants/feature.ts
@@ -0,0 +1,24 @@
+import EditIcon from "@/assets/icons/edit-icon.svg";
+import DocumentIcon from "@/assets/icons/document-icon.svg";
+import TrackingIcon from "@/assets/icons/tracking-icon.svg";
+
+export const FEATURES = [
+  {
+    icon: EditIcon,
+    title: "Edit \n Akses Dokumen",
+    description:
+      "Kamu dapat membuka serta menghapus akses pengguna lain terhadap dokumenmu",
+  },
+  {
+    icon: DocumentIcon,
+    title: "Ajuan Dokumen",
+    description:
+      "Kamu dapat mengajukan dokumenmu untuk dapat disimpan ke dalam website",
+  },
+  {
+    icon: TrackingIcon,
+    title: "Tracking Riwayat Akses",
+    description:
+      "Kamu dapat melihat siapa saja yang mengakases dokumenmu di dalam website",
+  },
+];
