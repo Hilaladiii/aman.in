780be243df5d71fac47e6c1c8a50b3a62ef0d6e9
Hilaladiii <abdulhilal242@gmail.com>
2024-09-22 17:18:13 +0700

    style(footer) : add shadow footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import Logo from "@/assets/icons/aman.in-icon.svg";
 
 export default function Footer() {
   return (
-    <footer className="shadow-footer mt-16 px-20 py-7">
-      <div className="flex flex-col items-start gap-4">
+    <footer className="bg-white shadow-footer">
+      <div className="flex flex-col items-start gap-4 px-20 py-7">
         <Logo className="size-10" />
         <h1 className="max-w-md text-left text-24 font-semibold">
           <span className="text-baseblack">
@@ -12,8 +12,8 @@ export default function Footer() {
           </span>
           <span className="text_logo px-3 py-1 text-24">aman.in</span>
         </h1>
-        <div className="text-neutral900 text-16">aman.in, 2024.</div>
-        <hr className="text-baseblack" />
+        <div className="text-16 text-neutral900">aman.in, 2024.</div>
+        <hr className="h-[1px] w-full bg-neutral500" />
         <div className="flex w-full flex-row items-center justify-between text-14 text-baseblack">
           <div>© 2024 aman.in</div>
           <div className="space-x-10">
