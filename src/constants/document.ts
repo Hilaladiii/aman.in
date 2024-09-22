791835b5fda83db3c89a7d9408566f242db455d5
Hilaladiii <abdulhilal242@gmail.com>
2024-09-22 17:20:20 +0700

    feat : add constant document for option document

---
+++ src/constants/document.ts
@@ -0,0 +1,24 @@
+export enum OPTION {
+  SIM = "SIM",
+  KTP = "KTP",
+  AKTA = "AKTA",
+  LAINNYA = "LAINNYA",
+}
+export const OPTIONDOCUMENTS = [
+  {
+    name: "Kartu Tanda Penduduk",
+    id: "KTP",
+  },
+  {
+    name: "Akta Kelahiran",
+    id: "AKTA",
+  },
+  {
+    name: "Surat Izin Mengemudi",
+    id: "SIM",
+  },
+  {
+    name: "Lainnya",
+    id: "LAINNYA",
+  },
+];
