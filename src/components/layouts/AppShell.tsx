fae57115ce92f4eacc25b1d67c7c440502da571f
Hilaladiii <abdulhilal242@gmail.com>
2024-09-27 19:21:12 +0700

    feat : add app shell layout

--- src/components/layouts/AppShell.tsx
+++ src/components/layouts/AppShell.tsx
@@ -5,7 +5,7 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <main>
       <Navbar />
-      {children}
+      <div className="min-h-max w-full pb-16">{children}</div>
       <Footer />
     </main>
   );
