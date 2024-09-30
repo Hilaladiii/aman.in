be223ae8105618ce0cb7d9f23b45df3396ac1066
Hilaladiii <abdulhilal242@gmail.com>
2024-09-30 23:25:34 +0700

    chore: update status

--- src/components/ui/Status.tsx
+++ src/components/ui/Status.tsx
@@ -4,32 +4,32 @@ export default function Status({
   status,
   className,
 }: {
-  status: "VERIFIKASI" | "BERHASIL" | "DITOLAK";
+  status: 0 | 1 | -1;
   className?: string;
 }) {
   return (
     <div
       className={cn(
         "inline-flex items-center gap-2 rounded-lg px-4 py-2",
-        status == "VERIFIKASI" && "bg-info100",
-        status == "BERHASIL" && "bg-success100",
-        status == "DITOLAK" && "bg-error100",
+        status == 0 && "bg-info100",
+        status == 1 && "bg-success100",
+        status == -1 && "bg-error100",
         className,
       )}
     >
       <div className="text-14 font-semibold text-baseblack">
-        {status == "VERIFIKASI"
+        {status == 0
           ? "Proses verifikasi"
-          : status == "BERHASIL"
+          : status == 1
             ? "Berhasil diverifikasi"
             : "Verifikasi ditolak"}
       </div>
       <div
         className={cn(
           "size-[18px] rounded-[4px]",
-          status == "VERIFIKASI" && "bg-info500",
-          status == "BERHASIL" && "bg-success500",
-          status == "DITOLAK" && "bg-error500",
+          status == 0 && "bg-info500",
+          status == 1 && "bg-success500",
+          status == -1 && "bg-error500",
         )}
       />
     </div>
