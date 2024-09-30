abd9edeb13f2c84d9af5850858c9d78e967aeb4a
Hilaladiii <abdulhilal242@gmail.com>
2024-09-30 23:31:04 +0700

    feat: add document with modal intercepting route

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,22 +1,10 @@
-interface ModalProps {
-  children: React.ReactNode;
-  show: boolean;
-  onClose: () => void;
-}
-
-export default function Modal({ children, show, onClose }: ModalProps) {
+export default function Modal({ children }: { children: React.ReactNode }) {
   return (
     <div
-      className={`fixed inset-0 flex items-start justify-center bg-black bg-opacity-40 transition-opacity duration-300 overflow-y-scroll ${
-        show ? "opacity-100" : "pointer-events-none opacity-0"
-      }`}
-      onClick={onClose}
+      className={`fixed inset-0 z-50 flex items-start justify-center overflow-y-scroll bg-black bg-opacity-40 transition-opacity duration-300`}
     >
       <div
-        className={`mt-28 flex transform flex-col items-center justify-center rounded-2xl bg-white p-8 transition-transform duration-300 ${
-          show ? "scale-100" : "scale-95"
-        }`}
-        onClick={(e) => e.stopPropagation()}
+        className={`mt-28 flex transform flex-col items-center justify-center rounded-2xl bg-white p-8 transition-transform duration-300`}
       >
         {children}
       </div>
