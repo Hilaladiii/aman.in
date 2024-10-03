19fa201b7e8fbeedad812b68a87cf570a658df6c
Hilaladiii <abdulhilal242@gmail.com>
2024-10-03 13:19:46 +0700

    chore: change margin & overflow modal

--- src/shared/container/Modal/Modal.tsx
+++ src/shared/container/Modal/Modal.tsx
@@ -1,10 +1,10 @@
 export default function Modal({ children }: { children: React.ReactNode }) {
   return (
     <div
-      className={`fixed inset-0 z-50 flex items-start justify-center overflow-y-scroll bg-black bg-opacity-40 transition-opacity duration-300`}
+      className={`fixed inset-0 z-50 flex items-start justify-center overflow-y-auto bg-black bg-opacity-40 transition-opacity duration-300`}
     >
       <div
-        className={`mt-28 flex transform flex-col items-center justify-center rounded-2xl bg-white p-8 transition-transform duration-300`}
+        className={`mt-24 flex transform flex-col items-center justify-center rounded-2xl bg-white p-8 transition-transform duration-300`}
       >
         {children}
       </div>
